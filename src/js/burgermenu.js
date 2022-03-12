0883bfd359b6e35c106955d7498d6c9db2a549d6
damiaNR10 <damianrog96@gmail.com>
2022-03-12 23:46:21 +0100

    Burgermenu created; few styles updated

---
+++ src/js/burgermenu.js
@@ -0,0 +1,33 @@
+const burgerButton = document.querySelector('.burgermenu--js');
+const navigation = document.querySelector('.nav--js');
+const body = document.querySelector('body');
+const navigationLinks = document.querySelectorAll('.nav__a');
+let isMenuVisible = false;
+
+const openMenu = () => {
+    body.classList.add('no-scroll');
+    navigation.classList.add('nav--active');
+    isMenuVisible = true;
+}
+
+const closeMenu = () => {
+    body.classList.remove('no-scroll');
+    navigation.classList.remove('nav--active');
+    isMenuVisible = false;
+}
+
+if(burgerButton) burgerButton.addEventListener('click', () => {
+    console.log('click');
+    !isMenuVisible ? openMenu() : closeMenu();
+});
+
+if(navigationLinks) navigationLinks.forEach(navigationLink => {
+    navigationLink.addEventListener('click', () => {
+        if(body.classList.contains('no-scroll')) 
+            body.classList.remove('no-scroll');
+            isMenuVisible = false;
+        if(navigation.classList.contains('nav--active')) 
+            navigation.classList.remove('nav--active');
+            isMenuVisible = false;
+    });
+});
